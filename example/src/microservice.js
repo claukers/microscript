857d24be42b75f0845afef52eaa494eb74dcb1ae
claukers <claukers@melia>
2019-03-30 18:19:55 -0300

    better example comments

--- example/src/microservice.js
+++ example/src/microservice.js
@@ -14,19 +14,20 @@ module.exports = async (app) => {
 
   // JWT Protected api
   const api = new ProtectedRoute();
+  // GET /api/v1/version
   api.get("/version", async (req, res) => {
     await new APIResponse({
       version: 1
     }).send(res);
   });
-  // /api/post
+  // GET/POST/PATCH/DELETE /api/v1/post
   api.use("/post", new ModelRoute(new ModelService(db.models.post)).routes());
 
   // attach api to app
   app.use("/api/v1", api.routes());
 
   // un protected route
-  // /post ( only GET method allowed )
+  // GET /post ( only GET method allowed )
   app.use("/public/post", new ModelRoute(new ModelService(db.models.post), {
     allowedMethods: ["GET"]
   }).routes());
