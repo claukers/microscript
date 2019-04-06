382c3db7202cb9ff1ab0525304d3a37d101d8630
claukers <claukers@melia>
2019-04-06 00:12:05 -0300

    handling SequelizeEagerLoadingError as BadRequest

--- src/route/service.ts
+++ src/route/service.ts
@@ -61,6 +61,8 @@ export class ServiceRoute extends Route {
           new BadRequestResponse(e.message).send(res);
         } else if (e.name === "SequelizeValidationError") {
           new BadRequestResponse(e.message).send(res);
+        } else if (e.name === "SequelizeEagerLoadingError") {
+          new BadRequestResponse(e.message).send(res);
         } else {
           logger.error(e);
           new ErrorResponse(e.message).send(res);
