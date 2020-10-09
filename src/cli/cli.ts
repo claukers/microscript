33206297d0f3869f98450ba3a4ac303a5517280e
claukers <claukers@amelia>
2020-10-09 03:44:49 -0300

    Revert "miqro and @miqro/database separation"
    This reverts commit c9915a6f

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -5,6 +5,16 @@ import {main as init} from "@miqro/core/dist/cli/init";
 import {main as config} from "@miqro/core/dist/cli/config";
 import {main as configBash} from "@miqro/core/dist/cli/config-bash";
 import {main as configEnv} from "@miqro/core/dist/cli/config-env";
+//@miqro/database
+import {main as dbInit} from "@miqro/database/dist/cli/init";
+import {main as makeMigrations} from "@miqro/database/dist/cli/makemigrations";
+import {main as migrate} from "@miqro/database/dist/cli/migrate";
+import {main as consoleCMD} from "@miqro/database/dist/cli/console";
+import {main as autoMigrate} from "@miqro/database/dist/cli/automigrate";
+import {main as seed} from "@miqro/database/dist/cli/seed";
+import {main as undoSeed} from "@miqro/database/dist/cli/undo-seed";
+import {main as migrationStatus} from "@miqro/database/dist/cli/migration-status";
+import {main as createModel} from "@miqro/database/dist/cli/createmodel";
 //@miqro/runner
 import {main as start} from "@miqro/runner/dist/cli/start";
 import {main as startScript} from "@miqro/runner/dist/cli/start-script";
@@ -21,7 +31,20 @@ CLIUtil.cliFlow({
     cb: configBash,
     description: "outputs to stdout the config as a bash script"
   },
-  ["config-env"]: {cb: configEnv, description: "outputs to stdout the config as a env file"},
+  ["config-env"]: {cb: configEnv, description: "\toutputs to stdout the config as a env file"},
+  ["db:init"]: {cb: dbInit, description: "\t\tinit sequelize configuration."},
+  ["db:makemigrations"]: {
+    cb: makeMigrations,
+    description: "seeks changes in your models and creates migrations"
+  },
+  ["db:console"]: {cb: consoleCMD, description: "\truns a readline interface that send the input as a query"},
+  ["db:migrate"]: {cb: migrate, description: "\truns the migrations"},
+  ["db:automigrate"]: {cb: autoMigrate, description: "\truns makemigrations and migrate together"},
+  ["db:seed"]: {cb: seed, description: "\t\tseeds your db"},
+  ["db:undo-seed"]: {cb: undoSeed, description: "\tundo all seeds from your db"},
+  ["db:migration-status"]: {cb: migrationStatus, description: "..."},
+  ["db:createmodel"]: {cb: createModel, description: "\tcreates an example model"},
+
   ["start"]: {cb: start, description: "\tstarts a microservice"},
   ["start-script"]: {cb: startScript, description: "starts a script"},
   ["start-api"]: {cb: startApi, description: "starts an apirouter on a directory"},
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "@miqro/core";
 export * from "@miqro/handlers";
+export * from "@miqro/database";
 export * from "@miqro/runner";
+export * from "@miqro/modelhandlers";
